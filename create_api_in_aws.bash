98cb0b515dcdc76aac5bde487b6b6d30f07d906f
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-11-16 19:49:55 +0100

    go

--- create_api_in_aws.bash
+++ create_api_in_aws.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws apigateway create-rest-api --name "mandelbrot_api" --region us-east-1
+aws apigateway create-rest-api --name "mandelbrot_api" --region us-east-1 --endpoint-configuration types=REGIONAL
 
 API_ID=$(aws apigateway get-rest-apis --query "items[?name=='mandelbrot_api'].id" --output text)
 
