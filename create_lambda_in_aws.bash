7db72511438eb8dfbfb3a5174826a626f079713e
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-11-19 20:16:50 +0100

    go

--- create_lambda_in_aws.bash
+++ create_lambda_in_aws.bash
@@ -1,4 +1,4 @@
  aws lambda create-function --function-name mandelbrot --runtime provided.al2023 --role arn:aws:iam::$(aws sts get-caller-identity --query "Account" --output text):role/LabRole --architectures x86_64 --memory-size 2048 --zip-file fileb://lambda/build/mandelbrot.zip --handler mandelbrot --timeout 30
 
- aws lambda put-function-concurrency --function-name mandelbrot --reserved-concurrent-executions 100
+ aws lambda put-function-concurrency --function-name mandelbrot --reserved-concurrent-executions 200
 
---
+++ update_lambda_in_aws.bash
@@ -0,0 +1,2 @@
+aws lambda update-function-code --function-name your-function-name --zip-file fileb://lambda/build/mandelbrot.zip
+
